30971cb64aad940eee586bd6f772bdac94f9f5ba
paulogermanopg <paulogermano.person@outlook.com.br>
2025-07-29 15:10:19 -0300

    feat: atom details

--- src/reactLogo/index.tsx
+++ src/reactLogo/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Canvas, Circle, Oval, Group } from '@shopify/react-native-skia';
 import { View, StyleSheet } from 'react-native';
+import {
+  useSharedValue,
+  useDerivedValue,
+  withRepeat,
+  withTiming,
+  Easing,
+} from 'react-native-reanimated';
 
 const ReactLogo = () => {
   const size = 300;
@@ -8,9 +15,35 @@ const ReactLogo = () => {
   const strokeWidth = 7;
   const color = '#61DAFB';
 
+  // Animação da opacidade da luz
+  const glow = useSharedValue(0.3);
+
+  useEffect(() => {
+    glow.value = withRepeat(
+      withTiming(0.6, {
+        duration: 2000,
+        easing: Easing.inOut(Easing.ease),
+      }),
+      -1,
+      true,
+    );
+  }, [glow]);
+
+  // Cor com alfa animado (hex RGBA)
+  const animatedGlowColor = useDerivedValue(() => {
+    const alpha = Math.round(glow.value * 255)
+      .toString(16)
+      .padStart(2, '0');
+    return `${color}${alpha}`; // "#61DAFB" + alpha
+  });
+
   return (
     <View style={styles.container}>
       <Canvas style={{ width: size, height: size }}>
+        {/* Luz de fundo */}
+        <Circle cx={center} cy={center} r={93} color={animatedGlowColor} />
+
+        {/* Logo React */}
         <Group origin={{ x: center, y: center }} transform={[{ rotate: 0 }]}>
           <Oval
             width={180}
@@ -62,6 +95,7 @@ const ReactLogo = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#000',
     justifyContent: 'center',
     alignItems: 'center',
   },
